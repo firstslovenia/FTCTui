da91782ad32200e5b92cc4ae50080d0d1c68a4e1
kozabrada123 <natantriestoedit@gmail.com>
2025-05-14 10:42:00 +0200

    feat: add match sfx and timer logic

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,59 @@
-use std::fs::File;
+use std::{fs::File, time::Duration};
 
 use app::App;
-use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
+use r#match::MatchSFXHandler;
+use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, WriteLogger};
 
 pub mod app;
 pub mod ftc_dashboard;
 pub mod ftc_proto;
 pub mod input;
+pub mod r#match;
 pub mod network;
 pub mod renderers;
 pub mod robot;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
-    CombinedLogger::init(vec![WriteLogger::new(
-        LevelFilter::Trace,
-        Config::default(),
-        File::create("log.log").unwrap(),
-    )])
+    CombinedLogger::init(vec![
+        WriteLogger::new(
+            LevelFilter::Trace,
+            Config::default(),
+            File::create("log.log").unwrap(),
+        ),
+        TermLogger::new(
+            LevelFilter::Debug,
+            Config::default(),
+            simplelog::TerminalMode::Mixed,
+            simplelog::ColorChoice::Auto,
+        ),
+    ])
     .unwrap();
+
+    let current_match = r#match::Match {
+        start: std::time::Instant::now(),
+    };
+
+    let (sender, receiver) = tokio::sync::broadcast::channel(2);
+
+    let mut sfx_handler = MatchSFXHandler {
+        receiver,
+        thread: None,
+    };
+
+    tokio::spawn(async move { sfx_handler.handler_thread().await });
+
+    sender.send(Some(current_match)).unwrap();
+
+    loop {
+        let phase = current_match.phase();
+        let timer = current_match.remaining_in_phase();
+
+        log::info!("{:?}, {:.1?} left", phase, timer);
+        tokio::time::sleep(Duration::from_millis(100)).await;
+    }
+
+    return Ok(());
 
     let app = App::new().await;
 
---
+++ src/match.rs
@@ -0,0 +1,169 @@
+use std::time::Duration;
+
+use tokio::task::JoinHandle;
+
+/// Used for match timers and sfx
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Match {
+    /// When we started the match
+    pub start: std::time::Instant,
+}
+
+impl Match {
+    /// Returns the current phase of the match
+    pub fn phase(&self) -> MatchPhase {
+        let elapsed = self.start.elapsed();
+
+        if elapsed < MatchPhase::Autonomous.length() {
+            return MatchPhase::Autonomous;
+        }
+
+        if elapsed < (MatchPhase::Autonomous.length() + MatchPhase::Transition.length()) {
+            return MatchPhase::Transition;
+        }
+
+        if elapsed
+            < (MatchPhase::Autonomous.length()
+                + MatchPhase::Transition.length()
+                + MatchPhase::Teleop.length())
+        {
+            return MatchPhase::Teleop;
+        }
+
+        MatchPhase::None
+    }
+
+    /// Returns how much is left before the end of this phase
+    pub fn remaining_in_phase(&self) -> Duration {
+        let elapsed = self.start.elapsed();
+
+        match self.phase() {
+            MatchPhase::None => Duration::from_secs(0),
+            MatchPhase::Autonomous => MatchPhase::Autonomous.length() - elapsed,
+            MatchPhase::Transition => {
+                (MatchPhase::Autonomous.length() + MatchPhase::Transition.length()) - elapsed
+            }
+            MatchPhase::Teleop => {
+                (MatchPhase::Autonomous.length()
+                    + MatchPhase::Transition.length()
+                    + MatchPhase::Teleop.length())
+                    - elapsed
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum MatchPhase {
+    Autonomous,
+    Transition,
+    Teleop,
+
+    /// After the match has already ended
+    None,
+}
+
+impl MatchPhase {
+    /// Returns how many seconds the phase lasts
+    pub fn length_seconds(&self) -> u64 {
+        match *self {
+            Self::Autonomous => 30,
+            Self::Transition => 8,
+            Self::Teleop => 120,
+            MatchPhase::None => 0,
+        }
+    }
+
+    /// Returns a strongly-typed length of the phase
+    pub fn length(&self) -> Duration {
+        Duration::from_secs(self.length_seconds())
+    }
+}
+
+/// Plays SFX for an active match
+#[derive(Debug)]
+pub struct MatchSFXHandler {
+    /// A receiver for updating the active match
+    pub receiver: tokio::sync::broadcast::Receiver<Option<Match>>,
+
+    /// A join handle for the current player thread
+    pub thread: Option<JoinHandle<()>>,
+}
+
+impl MatchSFXHandler {
+    /// Plays the provided sound
+    pub async fn play_sound(sfx: MatchSFX) {
+        log::info!("Play {:?}", sfx);
+    }
+
+    /// A thread which plays audio by awaiting timeouts
+    /// for certain elapsed intervals
+    pub async fn player_thread(active_match: Match) {
+        MatchSFXHandler::play_sound(MatchSFX::MatchStart01).await;
+
+        let transition_start = active_match.start + MatchPhase::Autonomous.length();
+        tokio::time::sleep_until(transition_start.into()).await;
+
+        MatchSFXHandler::play_sound(MatchSFX::Drivers02).await;
+
+        let countdown_start =
+            transition_start + (MatchPhase::Transition.length() - Duration::from_secs(3));
+        tokio::time::sleep_until(countdown_start.into()).await;
+
+        MatchSFXHandler::play_sound(MatchSFX::Countdown03).await;
+
+        let teleop_start = transition_start + MatchPhase::Transition.length();
+        tokio::time::sleep_until(teleop_start.into()).await;
+
+        MatchSFXHandler::play_sound(MatchSFX::TeleopStart04).await;
+
+        let endgame_start = teleop_start + (MatchPhase::Teleop.length() - Duration::from_secs(30));
+        tokio::time::sleep_until(endgame_start.into()).await;
+
+        MatchSFXHandler::play_sound(MatchSFX::Endgame05).await;
+
+        let end = teleop_start + MatchPhase::Teleop.length();
+        tokio::time::sleep_until(end.into()).await;
+
+        MatchSFXHandler::play_sound(MatchSFX::End06).await;
+    }
+
+    /// Handles spawning player threads and killing them if we get a new match
+    pub async fn handler_thread(&mut self) {
+        loop {
+            match self.receiver.recv().await {
+                Ok(update) => match update {
+                    Some(new_match) => {
+                        let thread =
+                            tokio::spawn(async move { Self::player_thread(new_match).await });
+
+                        if let Some(old_thread) = &self.thread {
+                            old_thread.abort();
+                        }
+
+                        self.thread = Some(thread);
+                    }
+                    None => {
+                        if let Some(old_thread) = &self.thread {
+                            old_thread.abort();
+                        }
+                    }
+                },
+                Err(e) => {
+                    log::warn!("Failed to receive new match! {:?}", e);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum MatchSFX {
+    MatchStart01,
+    Drivers02,
+    Countdown03,
+    TeleopStart04,
+    Endgame05,
+    End06,
+    Abortmatch,
+}
