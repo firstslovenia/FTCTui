4a3b3c61fc6c21688404432b35d70004d24958cf
kozabrada123 <natantriestoedit@gmail.com>
2025-05-14 10:42:43 +0200

    remove timer tester

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::{fs::File, time::Duration};
+use std::fs::File;
 
 use app::App;
-use r#match::MatchSFXHandler;
-use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, WriteLogger};
+use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
 
 pub mod app;
 pub mod ftc_dashboard;
@@ -15,45 +14,12 @@ pub mod robot;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
-    CombinedLogger::init(vec![
-        WriteLogger::new(
-            LevelFilter::Trace,
-            Config::default(),
-            File::create("log.log").unwrap(),
-        ),
-        TermLogger::new(
-            LevelFilter::Debug,
-            Config::default(),
-            simplelog::TerminalMode::Mixed,
-            simplelog::ColorChoice::Auto,
-        ),
-    ])
+    CombinedLogger::init(vec![WriteLogger::new(
+        LevelFilter::Trace,
+        Config::default(),
+        File::create("log.log").unwrap(),
+    )])
     .unwrap();
-
-    let current_match = r#match::Match {
-        start: std::time::Instant::now(),
-    };
-
-    let (sender, receiver) = tokio::sync::broadcast::channel(2);
-
-    let mut sfx_handler = MatchSFXHandler {
-        receiver,
-        thread: None,
-    };
-
-    tokio::spawn(async move { sfx_handler.handler_thread().await });
-
-    sender.send(Some(current_match)).unwrap();
-
-    loop {
-        let phase = current_match.phase();
-        let timer = current_match.remaining_in_phase();
-
-        log::info!("{:?}, {:.1?} left", phase, timer);
-        tokio::time::sleep(Duration::from_millis(100)).await;
-    }
-
-    return Ok(());
 
     let app = App::new().await;
 
